2e77d9a179c3b8c57c3abdc10a3226e454a661c2
chiuzon <chiuzon@protonmail.com>
2022-03-25 15:13:23 +0200

    Node polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+
+const MODE = process.env.NODE_ENV;
+const development = MODE === "development";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    svelte(),
+    development &&
+      nodePolyfills({
+        include: [
+          "node_modules/**/*.js",
+          new RegExp("node_modules/.vite/.*js"),
+        ],
+      }),
+  ],
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+      assert: "assert",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills({})],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
