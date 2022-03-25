379a7fac7184ff40a5ddeea96b7150fb7aefb325
chiuzon <chiuzon@protonmail.com>
2022-03-25 15:49:18 +0200

    Example of link

--- src/components/composed/Layout.svelte
+++ src/components/composed/Layout.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+  import { link } from "svelte-spa-router";
+</script>
+
 <div>
+  <div>
+    <a href="/" class="text-blue-500" use:link>Home</a>
+  </div>
   <slot />
 </div>
--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import Seo from "@/components/primitives/Seo.svelte";
 </script>
+
+<Seo pageName="Home" />
 
 <h1 class="text-xl">Home</h1>
