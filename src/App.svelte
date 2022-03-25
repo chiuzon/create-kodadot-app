fd416c568ce63885a5fe9d3bfa173d798284744b
chiuzon <chiuzon@protonmail.com>
2022-03-25 15:43:14 +0200

    Router paths and Layout component

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,10 @@
 <script lang="ts">
+  import Router from "svelte-spa-router";
+  import Routes from "@/constants/routes";
+
+  import Layout from "@components/composed/Layout.svelte";
 </script>
+
+<Layout>
+  <Router routes={Routes} />
+</Layout>
